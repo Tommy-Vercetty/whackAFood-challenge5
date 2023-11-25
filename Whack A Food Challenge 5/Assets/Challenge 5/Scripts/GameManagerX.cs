c2c3672f221a50e2fae3aef51643cfedcbe363e3
Enrico Ortenzi <b00156016@mytudublin.ie>
2023-11-25 13:29:15 +0000

    ojh

--- Whack A Food Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Whack A Food Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -10,7 +10,9 @@ public class GameManagerX : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public GameObject titleScreen;
-    public Button restartButton; 
+    public Button restartButton;
+    public TextMeshProUGUI timer;
+    private float timeLeft;
 
     public List<GameObject> targetPrefabs;
 
@@ -23,14 +25,28 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
+        timeLeft = 60;
+    }
+
+    private void Update()
+    {
+        if (isGameActive)
+        {
+            timeLeft -= Time.deltaTime;
+            timer.SetText("Time: " + Mathf.Round((timeLeft)));
+            if (timeLeft < 0)
+            {
+                GameOver();
+            }
+        }
     }
 
     // While game is active spawn a random target
@@ -70,14 +86,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
